2945edb5c0fe4fd25788208b88471cd3ba57664e
Romina Sepullveda Leal <89989186+rominasepulvedaleal@users.noreply.github.com>
2024-01-18 15:18:25 +0100

    Add files via upload
    Enunciados módulo 2

---
+++ Ejemplos y ejercicios Módulo 2 Enunciados.sql
@@ -0,0 +1,126 @@
+/*
+SQL Nivel Inicial
+Ejemplos y ejercicios módulo 2
+*/
+
+-- Ejemplo: 
+
+-- Consultar una columnas  de una tabla
+
+
+
+-- Consultar múltiples columnas  de una tabla.
+
+
+	
+--	Consultar todas las columnas de una tabla
+
+
+	
+--Usa LIMIT para obtener 3 filas en el resultado
+
+
+
+
+-- Consulta la tabla rental ordenada según rental data
+-- ¿Qué fechas quedaron arriba?
+
+
+	
+-- Ejercicios		
+	
+-- Ejercicio 1 ¿Qué duración tienen las películas más largas
+-- 				(length) de table film?
+
+
+
+-- Ejercicio 2  ¿Cuál es el precio de renta (rental_rate) 
+--               mayor en la table film?
+
+
+
+
+-- Ejercicio 3 ¿Hay algún cliente que su nombre comience
+--             con la letra Z?
+
+
+
+-- Ejercicio 4 ¿Qué mes se realizó el primer arriendo de dvd (table rental)?
+
+
+
+
+
+-- Ejemplo clase. Consulta con una condición
+-- Consulta las películas con un rental_duration mayor a 5
+
+
+
+-- Consulta con múltiples condiciones
+-- Películas con rental_duration mayor a 4 y el language_id  igual a 1
+
+
+
+-- Operadores Lógicos
+-- Películas con rental_duration mayor a 4 
+-- y menor a 7 y que el clasificación (rating) no sea restringido para menores 
+
+
+
+
+-- Ejercicios
+
+-- Ejercicio 5: ¿Qué país tiene in id de 22?
+
+
+-- Ejercicio 6: Obten las nombre y valor de inventario (replacement_cost)
+-- de las películas que tengan un valor de inventario mayor o igual a 20
+
+
+
+
+-- Ejercicio 7: Obten las nombre y valor (replacement_cost) 
+-- de las peliculas que tengas un valor mayor o igual a 20 
+-- y menos o igual a 25
+
+
+
+/*
+Ejercicio 8: Obten las títulos y valores (replacement_cost) de las peliculas 
+que tengas un valor mayor o igual a 20 y menos o igual a 25. 
+Ordena los resultados de manera que los valores mayores aparezcan primero 
+y limita tu resultado a solo 4 filas.
+*/
+
+
+
+
+-- Ejemplos
+
+-- Obten los nombres de las categorias  que tienen 
+-- category_id 1, 14 o 16
+
+
+	
+-- Uso de IN para ejemplo anterior
+
+
+
+-- Obten los nombres de las categorías que no son populares
+
+
+
+
+-- Consulta las direcciones de la tabla address
+
+
+
+
+
+
+-- Consulta las direcciones de la tabla address
+-- excluyendo las que tienen valor nulo en la columna address2
+
+
+
+
